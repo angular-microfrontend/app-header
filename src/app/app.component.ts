96f3930f94a9d9e7f3156a274c7ffae9b9f2d41d
Jagat Jeevan Sahoo <jagatsah@thoughtworks.com>
2020-04-06 12:25:23 +0530

    Add translation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component, Input } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: "halodoc-header-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  languages = ["en", "id"];
+  currentLang;
   @Input() name: string;
 
-  constructor() {
+  constructor(private translate: TranslateService) {
     console.log(this.name);
+    const currLang = window.localStorage.getItem("lang");
+    console.log("from header module", currLang);
+    translate.use(currLang);
+    this.currentLang = currLang;
+  }
+
+  updateLanguageTranslation(lang: string) {
+    this.translate.use(lang);
+    window.localStorage.setItem("lang", lang);
+    this.currentLang = lang;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,47 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule, Injector } from "@angular/core";
 import { createCustomElement } from "@angular/elements";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { MultiTranslateHttpLoader } from "ngx-translate-multi-http-loader";
+import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
 
 import { AppComponent } from "./app.component";
 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new MultiTranslateHttpLoader(http, [
+    {
+      prefix: "http://localhost:7454/api/",
+      suffix: ".json",
+    },
+  ]);
+}
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+  ],
   providers: [],
   bootstrap: [],
-  entryComponents: [AppComponent]
+  entryComponents: [AppComponent],
 })
 export class AppModule {
   customElement;
   constructor(private injector: Injector) {
     this.customElement = createCustomElement(AppComponent, { injector });
-    customElements.define("halodoc-header-root", this.customElement);
+    if (!customElements.get("halodoc-header-root")) {
+      customElements.define("halodoc-header-root", this.customElement);
+    } else {
+      console.warn(`${name} has been defined twice`);
+    }
   }
 
   ngDoBootstrap() {
